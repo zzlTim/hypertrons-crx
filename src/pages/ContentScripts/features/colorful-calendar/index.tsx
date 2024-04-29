3c93c9a69607cbbc857aebd6ee3d5e6e6d3bd9f0
zzlTim <3502603706@qq.com>
2024-04-29 15:45:10 +0800

    改变了颜色

---
+++ src/pages/ContentScripts/features/colorful-calendar/index.tsx
@@ -0,0 +1,25 @@
+import features from '../../../../feature-manager';
+
+import * as pageDetect from 'github-url-detection';
+
+const featureId = features.getFeatureID(import.meta.url);
+
+const init = async (): Promise<void> => {
+  console.log('init colorful-calendar');
+};
+
+const restore = async () => {
+  console.log('restore colorful-calendar');
+  const root = document.documentElement;
+  root.style.setProperty('--color-calendar-graph-day-L1-bg', '#ffedf9');
+  root.style.setProperty('--color-calendar-graph-day-L2-bg', '#ffc3eb');
+  root.style.setProperty('--color-calendar-graph-day-L3-bg', '#ff3ebf');
+  root.style.setProperty('--color-calendar-graph-day-L4-bg', '#c70085');
+};
+
+features.add(featureId, {
+  asLongAs: [pageDetect.isUserProfile],
+  awaitDomReady: false,
+  init,
+  restore,
+});
